2f31dab05141a1fb6111c65eba2dd9916c669678
Maria M <mmm.dam2022@hotmail.com>
2024-02-20 00:42:50 +0100

    Finished touching up the php response page for sending the form

--- formulario incidencias/verificar.php
+++ formulario incidencias/verificar.php
@@ -5,7 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <?php if (!empty($_POST['nombre']) && !empty($_POST['apellidos'])) {
+    <?php
+    //Definiendo variables
+    $nombre = $_POST['nombre'];
+    $apellidos = $_POST['apellidos'];
+    $telefono = $_POST['telefono'];
+    $email = $_POST['email'];
+    $departamentos = $_POST['departamentos'];
+    $asunto = $_POST['asunto'];
+    $descripcion = $_POST['descripcion'];
+
+    if (!empty($nombre) && !empty($apellidos) && !empty($telefono) && !empty($email) && !empty($departamentos) && !empty($asunto) && !empty($descripcion)) {
         echo
         "<title>Datos recibidos</title>";
     } else {
@@ -16,18 +26,19 @@
 
 <body>
     <?php
-    if (!empty($_POST['nombre']) && !empty($_POST['apellidos']) && !empty($_POST['telefono']) && !empty($_POST['email']) && !empty($_POST['departamentos']) && !empty($_POST['asunto']) && !empty($_POST['descripcion'])) {
+    //Definiendo variables
+    $nombre = $_POST['nombre'];
+    $apellidos = $_POST['apellidos'];
+    $telefono = $_POST['telefono'];
+    $email = $_POST['email'];
+    $departamentos = $_POST['departamentos'];
+    $asunto = $_POST['asunto'];
+    $descripcion = $_POST['descripcion'];
+
+    if (!empty($nombre) && !empty($apellidos) && !empty($telefono) && !empty($email) && !empty($departamentos) && !empty($asunto) && !empty($descripcion)) {
         echo "<h2>Los datos se han recibido correctamente.</h2>
     <p>En breve nos pondremos en contacto con usted. A continuación puede revisar los datos que nos acaba de enviar:</p>";
-        //Definiendo variables
-        // variable: nombre = lo que se reciba por post con el div "nombre"
-        $nombre = $_POST['nombre'];
-        $apellidos = $_POST['apellidos'];
-        $telefono = $_POST['telefono'];
-        $email = $_POST['email'];
-        $departamentos = $_POST['departamentos'];
-        $asunto = $_POST['asunto'];
-        $descripcion = $_POST['descripcion'];
+
 
         //imprimiendo los datos enviados por el post usando las variables que acabamos d edefinir
         echo '<ul><b>Nombre:</b> ' . "$nombre<br>";
@@ -38,15 +49,13 @@
         echo '<b>Asunto: </b>' . "$asunto<br>";
         echo '<b>descripcion: </b>' . "$descripcion</ul>";
     } else {
-        // definir la variable que contiene el enlace para volver al formulario:
-        $aqui = '<a href="incidencias.php">aquí</a>';
         // redirección
         header('Refresh: 10, URL=incidencias.php');
         // mensaje
         echo '<h2>Faltan datos.</h2>
         <p>Por favor, vuelva a la página anterior y rellene el formulario correctamente con los datos que solicitados para que podamos atender su incidencia.</p>
         <p>A continuación, será redireccionado a la página interior. Revise sus datos y reenvienos su incidencia.</p>
-        <p>Si su navegador no le reedirecciona automáticamente en 10 segundos, pulse ' . $aqui . '.';
+        <p>Si su navegador no le reedirecciona automáticamente en 10 segundos, pulse <a href="incidencias.php">aquí</a>.';
     }
     ?>
 </body>
